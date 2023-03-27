a98cbba616770d8ab8b5dee894467a864f488a3e
laura-dms <123728986+laura-dms@users.noreply.github.com>
2023-03-27 08:09:43 +0200

    Update shapes.h

--- shapes.h
+++ shapes.h
@@ -1,13 +1,34 @@
-#ifndef POINT_H
+#ifndef POINT_H //avoid error of multiples definitions of file
 #define POINT_H
 
+//Structure Point
 typedef struct{
     int pos_x;
     int pos_y;
 }Point;
 
-Point *create_point(int px, int py); //Allocate
+//Definitions of the functions associated to the structure Point
+Point* create_point(int px, int py); //Allocate
 void delete_point(Point * point);
 void print_point(Point * p);
+
+//Structure Line
+
+typedef struct line {
+    Point *p1;
+    Point *p2;
+}Line;
+
+//Definitions of the functions associated to the structure Line
+Line *create_line(Point * p1, Point * p2);
+void delete_line(Line * line);
+void print_line(Line * line);
+
+//Structure Square
+
+typedef struct square {
+    Point *p1;
+    Point *p2;
+}Square;
 
 #endif
