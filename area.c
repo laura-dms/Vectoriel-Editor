0a3feb73858f704fb39148368a285a9bcfafbb56
laura-dms <123728986+laura-dms@users.noreply.github.com>
2023-05-06 17:12:04 +0200

    Mise à jour fonctions

--- area.c
+++ area.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include "list.h"
 #include "shapes.h"
+#include "Pixel_shape.h"
 
 //Functions about the structure area//
 Area* create_area(unsigned int width, unsigned int height) //create a zone of type are
@@ -11,10 +12,7 @@ Area* create_area(unsigned int width, unsigned int height) //create a zone of ty
     Area * area = (Area*)malloc(width*height*sizeof(Area));//allocate the memory necessary to store the array of pixels//
     area->width = width;
     area->height = height;
-
-    NODE ** list_shapes = NULL;//(Shape*)malloc(sizeof(Shape));
-    area->shapes=list_shapes; //allocate dynamically the list of shapes
-
+    area->shapes=NULL; //allocate dynamically the list of shapes
     return area;  //returns a pointer to area
 }
 
@@ -45,38 +43,23 @@ void erase_area(Area* area) /*delete all the shapes in the drawing area
 
 void delete_area(Area* area)
 {
+    erase_area(area);
     free(area->mat);
 }
 
 void draw_area(Area* area)
-{    //check if there is enough space in the area to store the shape
-    int length = 0, height=0;
+{
+    ////check if there is enough space in the area to store the shape ???
     for (int i=0; i<area->nb_shape; i++)
     {
-        /*switch(area->shapes[i]->shape_type) {
-            case 0:
-                length = 1;
-                height = 1;
-                print_point(area->shapes[i]->ptrShape); //use the functions from structures shape
-                break;
-            case 1:
-                height = 1;
-                //length = ?? //how to calculate the length
-                print_line(area->shapes[i]->ptrShape);
-                break;
-            case 2:
-                print_square(area->shapes[i]->ptrShape);
-                break;
-            case 3:
-                print_rectangle(area->shapes[i]->ptrShape);
-                break;
-            case 4:
-                print_circle(area->shapes[i]->ptrShape);
-                break;
-            case 5:
-                print_polygon(area->shapes[i]->ptrShape);
-        }*/
+        ////Run through the list of pixels to be coloured and set 0 or 1 to mat
+        Pixel**List_Pixels=create_shape_to_pixel(area->shapes[i]);
+        for (int j=0; j</*nb_pixels*/; j++)
+        {
+            area->mat[List_Pixels[j]->px][List_Pixels[j]->py]=1;
+        }
     }
+
 }
 
 void print_area(Area* area) //draw the 2D matrix with the shapes in the console
@@ -86,8 +69,6 @@ void print_area(Area* area) //draw the 2D matrix with the shapes in the console
      * (which will be described later in this document) which must
      * return the list of pixels to be coloured.
 
-    Run through the list of pixels to be coloured and set 0 or 1 to mat
-    Pixel**List_Pixels=create_shape_to_pixel(Shape* shape);
     for (int i=0; i<area->nb_shape; i++)
     {
         if (*List_Pixels[i]==0)
