b2b4d2056c89dc4f3ffaddbf276d51031e910409
¤ Imoka ¤ <123319789+Im0ka@users.noreply.github.com>
2023-05-10 17:10:07 +0200

    Update main.c

--- main.c
+++ main.c
@@ -4,29 +4,26 @@ File that contains the main code of the project*/
 #include <stdio.h>
 #include "menu.h"
 #include "list.h"
+#include "area.h"
 
 int main(){
     int m_state, nb_shape = 0;
-
-    NODE* shapes = NULL;
-
+    /*NODE* shapes = NULL;
 
     do{
         m_state = menu(&nb_shape, &shapes);
-    }while(m_state != 1);
+    }while(m_state != 1);*/
 
-     //Area
     Area* draw_zone = create_area(12, 14);
-    Shape* shape1 = create_line_shape(5, 5 , 10, 10);
-    Shape* shape2 = create_circle_shape(5, 5, 4);
+    Shape* shape1 = create_point_shape(8, 11);
     add_shape_to_area (draw_zone, shape1);
-    add_shape_to_area (draw_zone, shape2);
+    print_list(draw_zone->shapes);
+    clear_area(draw_zone);
+    draw_zone->mat[0][0] = 1;
     draw_area (draw_zone);
     print_area (draw_zone);
-    erase_area (draw_zone);
-    draw_area (draw_zone);
-    print_area (draw_zone);
+    //erase_area (draw_zone);
     delete_area (draw_zone);
-    
+
     return 0;
 }
