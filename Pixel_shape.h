529896df3488c00adfda75b4330e6b9b781846ef
¤ Imoka ¤ <123319789+Im0ka@users.noreply.github.com>
2023-05-13 12:10:32 +0200

    Update Pixel_shape.h

--- Pixel_shape.h
+++ Pixel_shape.h
@@ -6,14 +6,14 @@
 #ifndef PIXEL_SHAPE_H
 #define PIXEL_SHAPE_H
 
-void pixel_point(Point* point, Pixel** pixel, int* nb_pixels);
-void pixel_line(Line* line, Pixel** pixel, int* nb_pixels);
-void pixel_square(Square* square, Pixel** pixel, int* nb_pixels);
-void pixel_rectangle(Rectangle * rectangle, Pixel** pixel, int* nb_pixels);
-void pixel_circle(Circle * circle, Pixel** pixel, int* nb_pixels);
-void pixel_polygon(Polygon * polygon, Pixel** pixel, int* nb_pixels);
-void pixel_shape(Shape* shape, Pixel** pixel, int* nb_pixels);
+void pixel_point(Point* point, Pixel*** pixel, int* nb_pixels);
+void pixel_line(Line* line, Pixel*** pixel, int* nb_pixels);
+void pixel_square(Square* square, Pixel*** pixel, int* nb_pixels);
+void pixel_rectangle(Rectangle * rectangle, Pixel*** pixel, int* nb_pixels);
+void pixel_circle(Circle * circle, Pixel*** pixel, int* nb_pixels);
+void pixel_polygon(Polygon * polygon, Pixel*** pixel, int* nb_pixels);
+void pixel_shape(Shape* shape, Pixel*** pixel, int* nb_pixels);
 Pixel** create_shape_to_pixel(Shape * shape, int* nb_pixels);
-void delete_pixel_shape(Pixel** pixel, int* nb_pixels);
+void delete_pixel_shape(Pixel*** pixel, int* nb_pixels);
 
 #endif
