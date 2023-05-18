ef37d865eab3c4c29b36afbda4907474897acd34
¤ Imoka ¤ <123319789+Im0ka@users.noreply.github.com>
2023-05-18 18:51:25 +0200

    Changement fonction add_list

--- list.c
+++ list.c
@@ -6,25 +6,16 @@ File that contains the prototypes of the functions about the list of nodes*/
 #include <stdio.h>
 
 NODE* create_node(Shape* val){
-    NODE* new_node = (NODE*)malloc(sizeof(NODE)); //dynamic allocation
+    NODE* new_node = (NODE*)malloc(sizeof(NODE));
     new_node->value = val;
     new_node->succ = NULL;
     return new_node;
 }
 
-LIST add_end_list(LIST L, Shape* val){
+LIST add_to_list(LIST L, Shape* val){
     NODE* new = create_node(val);
-    if (L == NULL){
-        L = new;
-    }
-    else {
-        NODE* temp = L;
-        while (temp->succ != NULL){
-            temp = temp->succ;
-        }
-        temp->succ = new;
-    }
-    return L;
+    new->succ=L;
+    return new;
 }
 
 void print_list(LIST L){
