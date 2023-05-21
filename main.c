aaf194d50cca19f7a3e873cd26fae9fbfda755b6
¤ Imoka ¤ <123319789+Im0ka@users.noreply.github.com>
2023-05-21 11:52:09 +0200

    Update instructions main

--- main.c
+++ main.c
@@ -15,18 +15,20 @@ int main(){
     }while(m_state != 1);*/
 
     Area* draw_zone = create_area(30, 30);
-    Shape* shape2 = create_rectangle_shape(16,9,10,4);
+    Shape* shape = create_circle_shape(26, 26, 7);
+    Shape* shape2 = create_rectangle_shape(28,25,10,4);
     //Shape* shape3 = create_polygon_shape(3);
-    Shape* shape4 = create_square_shape(3,27,4);
+    //Shape* shape4 = create_square_shape(30,30,4);
     //add_shape_to_area(draw_zone, shape3);
     add_shape_to_area(draw_zone, shape2);
-    add_shape_to_area (draw_zone, shape4);
+    add_shape_to_area(draw_zone, shape);
+    //add_shape_to_area (draw_zone, shape4);
     print_list(draw_zone->shapes);
     printf("\n");
     clear_area(draw_zone);
     draw_area (draw_zone);
     print_area (draw_zone);
-    //erase_area (draw_zone);
+    erase_area (draw_zone);
     delete_area (draw_zone);
 
     return 0;
