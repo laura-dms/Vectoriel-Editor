f251ff1ea9441d8090e45fa210398d9ffad0b606
¤ Imoka ¤ <123319789+Im0ka@users.noreply.github.com>
2023-05-23 23:40:49 +0200

    FINAL VERSION

--- Pixel.h
+++ Pixel.h
@@ -1,13 +1,11 @@
 #ifndef PIXEL_H
 #define PIXEL_H
 
-struct pixel { //coordinates of the points to be filled in the drawing area
+struct pixel {
     int px;
     int py;
 };
 typedef struct pixel Pixel;
-
-//Definitions of functions about the structure Pixel
 
 Pixel* create_pixel(int px, int py);
 void delete_pixel(Pixel* pixel);
