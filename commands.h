a3b2e62d5b141b03fc4ef54f9a723ac451b50816
¤ Imoka ¤ <123319789+Im0ka@users.noreply.github.com>
2023-05-23 23:39:07 +0200

    FINAL VERSION

--- commands.h
+++ commands.h
@@ -1,22 +1,22 @@
 #ifndef COMMANDS_H
 #define COMMANDS_H
+#include "area.h"
 
 struct command {
-    char name[50];
+    char* name;
     int int_size;
-    int int_params[10];
+    int* int_params;
     int str_size;
-    char* str_params[10];
+    char** str_params;
 };
 typedef struct command Command;
 
-Command* create_commande();
-void add_str_param(Command* cmd, char* p);
+Command* create_command();
+//void add_str_param(Command* cmd, char* p);
 void add_int_param(Command* cmd, int p);
 void free_cmd(Command* cmd);
-int read_exec_command(Command* cmd);
-void read_from_stdin(Command* cmd);
-void split_strings(char str[]);
-void menu_commands();
+int read_exec_command(Command** cmd, Area** draw_zone);
+void read_from_stdin(Command** cmd);
+char** split_strings(char* str);
 
 #endif
