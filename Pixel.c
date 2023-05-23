23bdbb9bfd18d61a8dab927a474e0a91d4647fe4
¤ Imoka ¤ <123319789+Im0ka@users.noreply.github.com>
2023-05-23 23:40:27 +0200

    FINAL VERSION

--- Pixel.c
+++ Pixel.c
@@ -1,19 +1,13 @@
 #include "pixel.h"
 #include <stdlib.h>
 
-Pixel* create_pixel(int px, int py)
-/*allocate a memory space of type Pixel, initialize it
- * and return the address of the allocated memory space*/
-{
-    Pixel * pixel = (Pixel*)malloc(sizeof(Pixel)); //dynamic allocation
-
-    pixel->px=px; //fill the attributes of the structure pixel
+Pixel* create_pixel(int px, int py){
+    Pixel * pixel = (Pixel*)malloc(sizeof(Pixel));
+    pixel->px=px;
     pixel->py=py;
-
     return pixel;
 }
 
-void delete_pixel(Pixel* pixel)
-{
+void delete_pixel(Pixel* pixel){
     free(pixel);
 }
