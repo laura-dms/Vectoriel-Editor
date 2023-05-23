544ca559ea439932f9e095de937eb54b8954fce0
¤ Imoka ¤ <123319789+Im0ka@users.noreply.github.com>
2023-05-23 17:05:57 +0200

    MAJ

--- id.c
+++ id.c
@@ -1,5 +1,5 @@
-//Management of unique identifiers
-unsigned int global_id = 0; //unsigned int : type of data that can take 0 or positive integers as values
+#include "id.h"
+unsigned int global_id = 0;
 
 unsigned int get_next_id()
 {
