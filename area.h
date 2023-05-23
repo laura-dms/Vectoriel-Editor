992d6376eb8a844db59eb996de8a4385c452ebc8
¤ Imoka ¤ <123319789+Im0ka@users.noreply.github.com>
2023-05-23 17:47:27 +0200

    MAJ delete_area_in_shape

--- area.h
+++ area.h
@@ -19,7 +19,7 @@ typedef struct area Area;
 
 Area* create_area(unsigned int height, unsigned int width);
 void add_shape_to_area(Area* area, Shape* shape);
-void delete_shape_in_area(Area* area, int id);
+int delete_shape_in_area(Area* area, int id);
 void clear_area(Area* area);
 void erase_area(Area* area);
 void delete_area(Area* area);
